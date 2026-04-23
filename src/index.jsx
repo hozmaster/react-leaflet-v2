b50700c428a0ae53d2dd35f57f6fbc2f6c6f2363
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2026-04-23 22:38:29 +0300

    Remove typescript rlated files

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
--- src/vite.env.d.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2026.  Olli-Pekka Wallin All rights reserved
- */
-
-/// <reference types="vite/client" />
